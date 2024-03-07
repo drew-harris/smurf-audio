bacb766fa51c23e2e8f4ea392aade1e94981386e
Drew Harris <harrisd@smu.edu>
2024-03-07 15:38:41 -0600

    Database connection

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	godotenv.Load()
+
+	db_url := os.Getenv("DATABASE_URL")
+	if db_url == "" {
+		panic("No database url loaded")
+	}
+	db, err := sqlx.Open("postgres", db_url)
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not open database")
+	}
+	fmt.Println(db.DB.Stats().InUse)
+
 }
